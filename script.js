c8d25114831805982cdfc14da30eb860649be8ed
sa <sm.anas@outlook.com>
2021-02-18 14:47:22 +0530

    Add function to display books
    Added a function to display books on the table
    - It creates a tr elemen
    - It create multiple td elements for title, author, page, and read
      status

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let books = []
 
+
 function Book( title, author, pages, readStatus ){
   this.title = title;
   this.author = author;
@@ -28,4 +29,39 @@ function createForm(){
   let table = document.getElementById('table');
   table.appendChild(form);
 }
+
+function displayBook(book){
+  let tableRow = document.createElement("tr");
+  let bookTitle = document.createElement("td");
+  bookTitle.textContent = book.title;
+  let bookAuthor = document.createElement("td");
+  bookAuthor.textContent = book.author;
+  let bookPages = document.createElement("td");
+  bookPages.textContent = book.pages;
+  let readStatus = document.createElement("td");
+  readStatus.textContent = book.readStatus;
+  let buttonTd = document.createElement("td");
+  let buttonContainer = document.createElement('div');
+  let readButton = document.createElement("button");
+  readButton.textContent = "Read";
+  let removeButton = document.createElement("button");
+  removeButton.textContent = "Delete";
+
+  buttonContainer.appendChild(readButton);
+  buttonContainer.appendChild(removeButton);
+  buttonTd.appendChild(buttonContainer);
+
+
+  let bookRecords = [ bookTitle, bookAuthor, bookPages, readStatus, buttonTd]
+  for( let i = 0; i < bookRecords.length; i++){
+    tableRow.appendChild(bookRecords[i]);
+  }
+
+  let table = document.getElementById('table');
+  table.appendChild(tableRow);
+}
+
+let newBook = new Book("Anythin", "Anyone", 245, true);
+books.push(newBook);
+
 
